4e8513d13efba2e1bfb613ba335d53df2f442518
Alexandre Ubaldo <alexandreubaldo@gmail.com>
2022-06-04 11:09:51 -0300

    Criação da tabela de pátios

---
+++ database/migrations/2022_06_04_134932_create_yards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('yards', function (Blueprint $table) {
+            $table->id();
+            $table->string('locator')->max(3)->unique();
+            $table->integer('length');
+            $table->integer('width');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yards');
+    }
+};
